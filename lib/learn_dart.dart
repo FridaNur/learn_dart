5fb9e13385c1e7a204e8b0e1795b3f3fb5ac3f0a
Rofiqul Umma <rofiqulumma@gmail.com>
2025-04-05 16:54:33 +0700

    if else

--- lib/learn_dart.dart
+++ lib/learn_dart.dart
@@ -1,3 +1,11 @@
-int calculate() {
-  return 6 * 7;
+void main() {
+  int uang = 5000;
+
+  if (uang > 5000) {
+    print(' Banyak bangett');
+  } else if (uang < 5000) {
+    print('dikit bangett');
+  } else {
+    print(' kerja boss ');
+  }
 }
