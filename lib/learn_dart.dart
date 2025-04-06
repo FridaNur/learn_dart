eb80744612a13122c8b4b0a920e492335975a569
Rofiqul Umma <rofiqulumma@gmail.com>
2025-04-06 15:10:04 +0700

    function_segitiga

--- lib/learn_dart.dart
+++ lib/learn_dart.dart
@@ -1,10 +1,12 @@
-void luasKubus(panjang, lebar) {
-  print(
-      ' Hasil hitung kubus yang memiliki $panjang dan $lebar adalah ${panjang * lebar}');
+int luasSegitiga(int panjang, int lebar, int tinggi) {
+  return panjang * lebar * tinggi;
 }
 
 void main() {
-  var panjang = 5;
-  var lebar = 7;
-  luasKubus(panjang, lebar);
+  int panjang = 8;
+  int lebar = 9;
+  int tinggi = 10;
+  int hasil = luasSegitiga(panjang, lebar, tinggi);
+  print(
+      ' Hasil hitung luas segitiga yang memiliki panjang $panjang, dan lebar $lebar, dan tinggi $tinggi adalah $hasil');
 }
