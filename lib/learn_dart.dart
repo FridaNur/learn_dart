263f280ee920507349db35643535f440c41dffc7
Rofiqul Umma <rofiqulumma@gmail.com>
2025-04-07 14:32:45 +0700

    for_in

--- lib/learn_dart.dart
+++ lib/learn_dart.dart
@@ -1,12 +1,23 @@
-int luasSegitiga(int panjang, int lebar, int tinggi) {
-  return panjang * lebar * tinggi;
+void main() {
+  tampilList();
 }
 
-void main() {
-  int panjang = 8;
-  int lebar = 9;
-  int tinggi = 10;
-  int hasil = luasSegitiga(panjang, lebar, tinggi);
-  print(
-      ' Hasil hitung luas segitiga yang memiliki panjang $panjang, dan lebar $lebar, dan tinggi $tinggi adalah $hasil');
+void forLoops() {
+  // loop angka 1 kuarang dari atau sama dengan 50
+  for (int i = 1; i < 50; i++) {
+    // i dibagi 5 maka menghasilkan bilangan desimal
+    var hasil = i / 5;
+    print(' Hasil $i dibagi 5 adalah $hasil');
+  }
+}
+
+void tampilList() {
+  List listNama = ["Tono", "Toni", "Tini", "Linda", "Martha", "Agus"];
+  //index 2 akan mwnghapus nama kalimat kedua
+  listNama.removeAt(2);
+  //menambahkan nama
+  listNama[1] = "Narto";
+  for (String nama in listNama) {
+    print(nama);
+  }
 }
